becd2827629756044a0f2ed109c95def8e598532
Rafael Barbosa <91401878+rafaelbarbos@users.noreply.github.com>
2026-04-23 20:40:34 -0300

    Add Atividade.java with main method

---
+++ example/hello/Atividade.java
@@ -0,0 +1,8 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public class Atividade {
+    public static void main(String[] args) {
+        System.out.println("Atividade concluida");
+    }
+}
